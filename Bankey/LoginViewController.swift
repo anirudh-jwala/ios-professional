f1abd8cf13907351f6854edba2412073c7b5ecfd
Anirudh Jwala <anirudhsai2@gmail.com>
2022-07-23 00:48:25 +0530

    Creating login view

---
+++ Bankey/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Anirudh on 23/07/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    let loginView = LoginView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        style()
+        layout()
+    }
+
+}
+
+extension LoginViewController {
+    private func style() {
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func layout() {
+        view.addSubview(loginView)
+        
+        NSLayoutConstraint.activate([
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.leadingAnchor.constraint(lessThanOrEqualToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1),
+        ])
+    }
+}
