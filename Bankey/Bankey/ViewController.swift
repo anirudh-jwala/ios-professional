c9d5c1d16cbbf30d13bf49ff586b62e1bf9b336a
Anirudh Jwala <anirudhsai2@gmail.com>
2022-08-29 11:31:46 +0530

    Base setup without storyboard

---
+++ Bankey/Bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Apple on 29/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
